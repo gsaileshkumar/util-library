f16990856f6dbe99b2ebcea3ed6184bf7ee898b5
gsaileshkumar <gsaileshkumar@gmail.com>
2019-09-27 02:23:55 +0800

    Added babel config for resolving react

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,39 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
 const dist = 'dist';
 const bundle = 'bundle';
+const input = 'src/index.js';
+const output = [
+  {
+    file: `${dist}/${bundle}.esm.js`,
+    format: 'esm',
+  },
+  {
+    file: `${dist}/${bundle}.cjs.js`,
+    format: 'cjs',
+  },
+  {
+    file: `${dist}/${bundle}.umd.js`,
+    format: 'umd',
+    name: 'Util',
+  },
+  {
+    file: `${dist}/${bundle}.iife.js`,
+    format: 'iife',
+    name: 'Util',
+  },
+];
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: `${dist}/${bundle}.esm.js`,
-      format: 'esm',
-    },
-    {
-      file: `${dist}/${bundle}.cjs.js`,
-      format: 'cjs',
-    },
-    {
-      file: `${dist}/${bundle}.umd.js`,
-      format: 'umd',
-      name: 'Util',
-    },
-    {
-      file: `${dist}/${bundle}.iife.js`,
-      format: 'iife',
-      name: 'Util',
-    },
+  input,
+  output,
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    terser(),
   ],
 };
